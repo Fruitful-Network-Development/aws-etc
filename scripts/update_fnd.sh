8093b5e21d69dbefd043dc9689657dcb5eb8c577
Dylan Montgomery <147453930+dylcmonty@users.noreply.github.com>
2025-12-06 16:17:40 -0500

    Update and rename update_code.sh to update_fnd.sh

--- scripts/update_fnd.sh
+++ scripts/update_fnd.sh
@@ -5,6 +5,4 @@ set -euo pipefail
 PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$PROJECT_ROOT"
 
-echo "=== Updating git repository in $PROJECT_ROOT ..."
 git pull --ff-only
-echo "=== Git update complete."
